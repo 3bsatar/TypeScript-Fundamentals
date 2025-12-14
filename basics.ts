f51b4a1a99f72377449879e299fa8942ad4e927f
Mahmoud Mesalem <mahmoudmesalem23@gmail.com>
2025-12-14 14:48:44 +0200

    Variables in Ts

--- basics.ts
+++ basics.ts
@@ -1,1 +1,12 @@
 console.log("Hello World!");
+
+let firstName = "Mahmoud";
+var lastName = "Mesalem";
+const age = 23;
+
+console.log("My name is "+firstName+" "+lastName+" and my age is "+age);
+
+firstName = "mahmoud";
+lastName = "mesalem";
+
+console.log(`My name is ${firstName} ${lastName} and my age is ${age}`);
