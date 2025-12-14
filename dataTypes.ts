809a519321c542191ba74a3e790274f30cf055e9
Mahmoud Mesalem <mahmoudmesalem23@gmail.com>
2025-12-14 14:58:02 +0200

    DataTypes in Ts

---
+++ dataTypes.ts
@@ -0,0 +1,36 @@
+// Boolean
+let isDone: boolean = true;
+let isNotDone: boolean = false;
+
+console.log(isDone);
+console.log(isNotDone);
+
+// number
+let num1: number = 15;
+let num2: number = 9123.12324343;
+
+console.log(num1);
+console.log(num2);
+
+// string
+let name: string = "Mahmoud";
+let number: string = "23";
+
+console.log(name);
+console.log(number);
+
+// Any
+let dynamic: any = "string";
+console.log(dynamic);
+dynamic = 17;
+console.log(dynamic);
+dynamic = false;
+console.log(dynamic);
+
+// null 
+let nullValue: null = null;
+console.log(nullValue);
+
+// undefined
+let undefinedValue: undefined = undefined;
+console.log(undefined);
