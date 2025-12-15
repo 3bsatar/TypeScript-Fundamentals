4773830777b02cad24e0532df5ba3c59bf53971f
Mahmoud Mesalem <mahmoudmesalem23@gmail.com>
2025-12-15 17:17:07 +0200

    Classes and Objects

---
+++ OOPInTs.ts
@@ -0,0 +1,26 @@
+
+// Classes & Object
+// A class is a blueprint for creating objects. An object is an instance of a class.
+class Animal {
+    animalName = '';
+
+    greeting() {
+        console.log(`Hello there, I'm a ${this.animalName}`)
+    }
+    move() {
+        console.log(`I can walk`)
+    }
+}
+const goat = new Animal();
+goat.animalName='Goat';
+goat.greeting();
+goat.move();
+
+// 2. Inheritance
+// Inheritance allows one class to extend another.
+
+
+// 3. Encapsulation
+// Encapsulation hides internal state and exposes only what's necessary.
+// 4. Polymorphism
+// Polymorphism lets you treat different classes through a common interface or base class.
